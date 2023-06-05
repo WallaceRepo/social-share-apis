6d2632894d839f2014d5bc72d6ac1db5e7bbe272
WallaceRepo <sophiwebhub@gmail.com>
2023-06-04 23:16:13 -0500

    removed oauth to be the parameter in main.go router

--- server/controllers/shareProductController.go
+++ server/controllers/shareProductController.go
@@ -6,13 +6,30 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/facebook"
+	"github.com/wallacerepo/social-share-apis/initializers"
 )
 
-func shareProduct(c *gin.Context, oauthConfig *oauth2.Config) {
+var (
+	oauthConfig *oauth2.Config
+)
+
+func ShareProduct (c *gin.Context) {
+
+	_, APP_ID, APP_SECRET, REDIRECT_URL := initializers.LoadEnvVariables()
+
+	// Initialize the OAuth2 configuration
+	oauthConfig = &oauth2.Config{
+		ClientID:     APP_ID,
+		ClientSecret: APP_SECRET,
+		RedirectURL:  REDIRECT_URL,
+		Scopes:       []string{"publish_actions"},
+		Endpoint:     facebook.Endpoint,
+	}
 	// Parse the request JSON
 	var body struct {
-		Product  string
-		Title string
+		Product string
+		Title   string
 	}
 	if err := c.BindJSON(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
@@ -23,7 +40,8 @@ func shareProduct(c *gin.Context, oauthConfig *oauth2.Config) {
 	authURL := oauthConfig.AuthCodeURL("state", oauth2.AccessTypeOnline)
 	c.Redirect(http.StatusFound, authURL)
 }
-func handleCallback(c *gin.Context, oauthConfig *oauth2.Config) {
+
+func HandleCallback(c *gin.Context) {
 	// Retrieve the authorization code from the query parameters
 	code := c.Query("code")
 
@@ -39,8 +57,7 @@ func handleCallback(c *gin.Context, oauthConfig *oauth2.Config) {
 
 	// Create the post message
 	// post := fmt.Sprintf("Check out this amazing product: %s", product.Name)
-	post := fmt.Sprintf("Check out this amazing product" )
-
+	post := fmt.Sprintf("Check out this amazing product")
 
 	// Share the post
 	err = shareToFacebook(accessToken, post)
